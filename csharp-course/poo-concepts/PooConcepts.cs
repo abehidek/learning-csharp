2cd79d1dff8880dd3292f4b7f53a405e2df0c60a
abehidek <hidek.abe@outlook.com>
2021-03-05 16:42:01 -0300

    Getters and Setters

--- csharp-course/poo-concepts/PooConcepts.cs
+++ csharp-course/poo-concepts/PooConcepts.cs
@@ -10,6 +10,13 @@ namespace csharp_course.poo_concepts
     {
         public static void AllPooConcepts()
         {
+            // GET AND SET
+
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+            shrek.Rating = "Dog";
+            Console.WriteLine(shrek.Rating);
+
             //OBJECT METHODS
             //Student student1 = new Student("Adam", "Physics", 3.2);
             //Student student2 = new Student("Beatrice", "Literature", 3.9);
@@ -17,7 +24,7 @@ namespace csharp_course.poo_concepts
             //Console.WriteLine(student1.HasHonnors());
             //Console.WriteLine(student2.HasHonnors());
 
-            //Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
