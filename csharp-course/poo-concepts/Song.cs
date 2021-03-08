8dce32a896e1b5f48ac2b665b74e0821265fd387
abehidek <hidek.abe@outlook.com>
2021-03-08 11:55:48 -0300

    Static Class and Methods

---
+++ csharp-course/poo-concepts/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_course.poo_concepts
+{
+    class Song
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        public static int songCount = 0; // STATIC CLASS ATRIBUTTE -> is a atribute about the class, not of the object
+
+        public Song(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            songCount++;
+        }
+    }
+}
---
+++ csharp-course/poo-concepts/UsefulTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_course.poo_concepts
+{
+    static class UsefulTools // IF A CLASS IS STATIC THEN IT CANNOT BE ABLE TO CREATE A NEW OBJECT OF THAT CLASS
+    {
+        public static void SayHi (string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
